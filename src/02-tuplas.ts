84d0658bde5931346015ebc7a87c6b0e80afb210
ja54312 <ija54312@gmail.com>
2022-09-19 17:07:53 -0500

    tuplas

---
+++ src/02-tuplas.ts
@@ -0,0 +1,8 @@
+//tuplas tipado de posicion y caracteres exactos
+
+const prices : (number | string) [] = [1,2,3,'as']
+prices.push(1)
+prices.push('q')
+console.log(prices,'precios')
+
+const user :  [string,number] = ['JA54312',15]
