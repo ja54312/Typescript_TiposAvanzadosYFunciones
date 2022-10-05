e2c37e3045b750803cadd8fa9d6b5dbe3d1e4657
ja54312 <ija54312@gmail.com>
2022-10-05 12:24:32 -0500

    read only

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -3,5 +3,7 @@ import {Product} from './product.model'
 const products:Product[] =[]
 
 export const addProduct = (data:Product) => {
+  //data.id = 'serwerwerwerwer'
+  //data.createdAt = new Date (1998,1,1)
   products.push(data)
 }
